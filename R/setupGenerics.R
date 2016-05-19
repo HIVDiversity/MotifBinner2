4dc9e82c0d936b47660635624f3eab834176dadf
Phillip Labuschagne <philliplab@gmail.com>
2016-05-19 15:44:11 +0200

    move reporting functions out of generics script

--- R/setupGenerics.R
+++ R/setupGenerics.R
@@ -8,42 +8,6 @@ saveToDisk <- function(result, config)
   UseMethod('saveToDisk')
 }
 
-#' Generates a report for a operation or the full process
-#' @inheritParams saveToDisk
-#' @export
-
-genReport <- function(result, config)
-{
-  new_report_name_md <- paste(config$prefix_for_names, 'bin_report.md', sep = '_')                         
-  new_report_name_html <- paste(config$prefix_for_names, 'bin_report.html', sep = '_')                     
-  new_report_name_pdf <- paste(config$prefix_for_names, 'bin_report.pdf', sep = '_')                       
-
-  report_file_name <- file.path(find.package('MotifBinner2'),
-                                'reports',
-                                paste(class(result), '.Rmd', sep = ''))
-  if (!file.exists(report_file_name))
-  {
-    report_file_name <- file.path(find.package('MotifBinner2'),
-                                  'inst', 'reports',
-                                  paste(class(result), '.Rmd', sep = ''))
-  }
-
-  cwd <- getwd()                                                                                    
-  setwd(output)                                                                                     
-  knit(knitr_file_location, new_report_name_md)                                                     
-
-  if ('html' %in% config$report_type){
-    render(report_file_name, output_format = 'html_document',
-           output_file = new_report_name_html, clean=FALSE)
-  }
-  if ('pdf' %in% config$report_type){
-    render(report_file_name, output_format = 'pdf_document',
-           output_file = new_report_name_pdf, clean=FALSE)
-  }
-  setwd(cwd)
-  return(result)
-}
-
 #' Generates a summary for a operation or the full process
 #' @inheritParams saveToDisk
 #' @export
