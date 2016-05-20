bdb89fd61e56ef1aeb1a72498363ff012db1f966
Phillip Labuschagne <philliplab@gmail.com>
2016-05-20 10:26:12 +0200

    handle summary calculation edge case of empty dataset

--- R/genSummary.R
+++ R/genSummary.R
@@ -20,25 +20,38 @@ genSummary_internal <- function(operation, parameters, good_seq_dat, bad_seq_dat
 
 genSummary_internal_one <- function(seq_dat)
 {
-  read_widths <- width(seq_dat)
-  seq_qual <- seq_dat@quality
-  per_read_quality <- letterFrequency(seq_qual@quality , names(encoding(seq_qual)))
-  per_read_quality <- (per_read_quality %*% matrix(encoding(seq_qual), ncol=1))[,1]
-  per_read_quality <- per_read_quality / read_widths
-  conMat <- consensusMatrix(seq_dat@sread)
-  tot_lets <- sum(conMat)
-  prop_gaps <- round(sum(conMat['-',])/tot_lets,5)
-  prop_non_ACGT <- round(sum(conMat[5:18,])/tot_lets,5)
-  prop_AT <- round(sum(conMat[c('A','T'),])/tot_lets,5)
+  if (length(seq_dat) == 0){
+    data.frame(seqs = 0,
+               q25_length = NA,
+               mean_length = NA,
+               q75_length = NA,
+               q25_qual = NA,
+               mean_qual = NA,
+               q75_qual = NA,
+               prop_gaps = NA,
+               prop_non_ACGT = NA,
+               prop_AT = NA)
+  } else {
+    read_widths <- width(seq_dat)
+    seq_qual <- seq_dat@quality
+    per_read_quality <- letterFrequency(seq_qual@quality , names(encoding(seq_qual)))
+    per_read_quality <- (per_read_quality %*% matrix(encoding(seq_qual), ncol=1))[,1]
+    per_read_quality <- per_read_quality / read_widths
+    conMat <- consensusMatrix(seq_dat@sread)
+    tot_lets <- sum(conMat)
+    prop_gaps <- round(sum(conMat['-',])/tot_lets,5)
+    prop_non_ACGT <- round(sum(conMat[5:18,])/tot_lets,5)
+    prop_AT <- round(sum(conMat[c('A','T'),])/tot_lets,5)
 
-  data.frame(seqs = length(seq_dat),
-             q25_length = quantile(read_widths, 0.25, names=FALSE),
-             mean_length = mean(read_widths, na.rm=TRUE),
-             q75_length = quantile(read_widths, 0.75, names=FALSE),
-             q25_qual = quantile(per_read_quality, 0.25, names = FALSE),
-             mean_qual = mean(per_read_quality, na.rm=T),
-             q75_qual = quantile(per_read_quality, 0.75, names = FALSE),
-             prop_gaps = prop_gaps,
-             prop_non_ACGT = prop_non_ACGT,
-             prop_AT = prop_AT)
+    data.frame(seqs = length(seq_dat),
+               q25_length = quantile(read_widths, 0.25, names=FALSE),
+               mean_length = mean(read_widths, na.rm=TRUE),
+               q75_length = quantile(read_widths, 0.75, names=FALSE),
+               q25_qual = quantile(per_read_quality, 0.25, names = FALSE),
+               mean_qual = mean(per_read_quality, na.rm=T),
+               q75_qual = quantile(per_read_quality, 0.75, names = FALSE),
+               prop_gaps = prop_gaps,
+               prop_non_ACGT = prop_non_ACGT,
+               prop_AT = prop_AT)
+  }
 }
