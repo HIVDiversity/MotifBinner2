f845de153cfc57e17a6ed7c8c132d3a73ffa4854
Phillip Labuschagne <philliplab@gmail.com>
2016-05-23 08:52:09 +0200

    add automated rebuilding of subreports to generate main report

--- R/genReport.R
+++ R/genReport.R
@@ -4,43 +4,13 @@
 
 genReport <- function(result, config)
 {
-  report_class_name <- class(result)
-  if (report_class_name == 'allResults')
+  if (class(result) == 'allResults')
   { 
-    report_class_name <- NULL
+    result <- genReport_internal_allResults(result, config)
+  } else {
+    result <- genReport_internal_generic(result, config)
   }
   
-  new_report_name_md   <- paste(config$prefix_for_names, report_class_name, 'bin_report.md', sep = '_')
-  new_report_name_html <- paste(config$prefix_for_names, report_class_name, 'bin_report.html', sep = '_')
-  new_report_name_pdf  <- paste(config$prefix_for_names, report_class_name, 'bin_report.pdf', sep = '_')
-
-  report_file_name <- file.path(find.package('MotifBinner2'),
-                                'reports',
-                                paste(class(result), '.Rmd', sep = ''))
-  if (!file.exists(report_file_name))
-  {
-    report_file_name <- file.path(find.package('MotifBinner2'),
-                                  'inst', 'reports',
-                                  paste(class(result), '.Rmd', sep = ''))
-  }
-
-  cwd <- getwd()
-  if (class(result) == 'all_results'){
-    setwd(file.path(config$output_dir))
-  } else {
-    setwd(result$op_dir)
-  }
-  knit(report_file_name, new_report_name_md)
-
-  if ('html' %in% config$report_type){
-    render(new_report_name_md, output_format = 'html_document',
-           output_file = new_report_name_html, clean=FALSE)
-  }
-  if ('pdf' %in% config$report_type){
-    render(new_report_name_md, output_format = 'pdf_document',
-           output_file = new_report_name_pdf, clean=FALSE)
-  }
-  setwd(cwd)
   return(result)
 }
 
@@ -60,7 +30,8 @@ genReport_internal_allResults <- function(result, config)
                                   paste(class(result), '.Rmd', sep = ''))
   }
 
-  setwd(file.path(config$output_dir))
+  cwd <- getwd()
+  setwd(file.path(config$output_dir, config$prefix_for_names))
   knit(report_file_name, new_report_name_md)
 
   if ('html' %in% config$report_type){
