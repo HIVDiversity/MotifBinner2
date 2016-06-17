6047a6256501978d92bdf05e7e6ea63037961d8a
Phillip Labuschagne <philliplab@gmail.com>
2016-06-17 12:34:14 +0200

    put genSummary where it belongs

--- R/genSummary.R
+++ R/genSummary.R
@@ -4,26 +4,55 @@
 
 genSummary <- function(result, config)
 {
-  UseMethod('genSummary')
+  op_number <- config$current_op_number
+  op_args <- config$operation_list[[op_number]]
+  op_full_name <- paste(op_number, op_args$name, sep = '_')
+
+  # load data
+  if (!op_args$cache)
+  {
+    stop('implement getKept now')
+  } else 
+  {
+    seq_dat <- result$seq_dat
+  }
+
+  for (trim_step in result$trim_steps)
+  {
+    trim_dat <- result$metrics$per_read_metrics[,trim_step$name,drop=T]
+    stopifnot(length(trim_dat) == length(seq_dat))
+    if (length(trim_step$breaks) == 1)
+    {
+      stopifnot(all(trim_dat == trim_step$threshold))
+      kept <- seq_dat
+      trimmed <- seq_dat[0]
+      parameter <- paste(trim_step$name, ' = ', trim_step$threshold, sep = '')
+      summary_tab <- genSummary_comb(kept = kept,
+                                     trimmed = trimmed,
+                                     op = class(result),
+                                     parameter = parameter)
+    } else {
+      stop('implement trimming with multiple categories now')
+    }
+  }
+  result$summary <- summary_tab
+  return(result)
 }
 
-#' Computes the summary tables of an operation
-#' @export
-
-genSummary_internal <- function(operation, parameters, kept_seq_dat, trimmed_seq_dat)
+genSummary_comb <- function(kept, trimmed, op, parameter)
 {
-  kept_tab <- genSummary_internal_one(kept_seq_dat)
-  names(kept_tab) <- paste(names(kept_tab), 'kept', sep = '_')
-  trimmed_tab <- genSummary_internal_one(trimmed_seq_dat)
-  names(trimmed_tab) <- paste(names(trimmed_tab), 'trimmed', sep = '_')
-  cbind(data.frame(operation = operation,
-                   parameters = parameters),
-        kept_tab, trimmed_tab)
+  cbind(
+    data.frame(op = op,
+               parameter = parameter),
+    genSummary_one(kept, 'k'),
+    genSummary_one(trimmed, 't')
+    )
 }
 
-genSummary_internal_one <- function(seq_dat)
+genSummary_one <- function(seq_dat, names_pref)
 {
   if (length(seq_dat) == 0){
+    ret_val <-
     data.frame(seqs = 0,
                q25_length = NA,
                mean_length = NA,
@@ -46,6 +75,7 @@ genSummary_internal_one <- function(seq_dat)
     prop_non_ACGT <- round(sum(conMat[5:18,])/tot_lets,5)
     prop_AT <- round(sum(conMat[c('A','T'),])/tot_lets,5)
 
+    ret_val <-
     data.frame(seqs = length(seq_dat),
                q25_length = round(quantile(read_widths, 0.25, names=FALSE),2),
                mean_length = round(mean(read_widths, na.rm=TRUE),2),
@@ -57,7 +87,10 @@ genSummary_internal_one <- function(seq_dat)
                prop_non_ACGT = round(prop_non_ACGT,4),
                prop_AT = round(prop_AT,4))
   }
+  names(ret_val) <- paste(names_pref, names(ret_val), sep = '_')
+  return(ret_val)
 }
+
 
 #' formats a summary table for markdown
 #' @param summary_tab A data.frame as produced by genSummary
